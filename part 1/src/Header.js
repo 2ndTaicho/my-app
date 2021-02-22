225e427a9b7d445f93e7a518d83292f232ad062b
2ndTaicho <sumaiyah.domingo@gmail.com>
2021-02-22 14:20:18 +0200

    first commit

---
+++ part 1/src/Header.js
@@ -0,0 +1,10 @@
+  
+import React from 'react';
+
+function Header(props) {
+	return (<header className="App-header"><h4>{props.loggedIn ? `Hello, ${props.name}` : ''}</h4>
+  	<h2>HEADER</h2></header>);
+}
+
+
+export default Header;
